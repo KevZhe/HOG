2bc4d85fc6ea9ec19b163c251ca18ec47bcaddd5
Kevin Lee <kl3642@nyu.edu>
2023-05-12 14:55:13 -0400

    initial work on KNN; not tested

---
+++ NearestNeighbor.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class KNearestNeighbor(object):
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+    
+    def _hellinger(self, X, S):
+        # S is an N x D matrix where each row is an example
+        # X is a single D-dimensional example
+
+        # Computer the Hellinger distance between the input vector and all examples in the training set
+        
+        return 1 - np.sum(np.sqrt(X * S), axis=1)
+
+    def _histogram_intersection(self, X, S):
+        return 1 - np.sum(np.minimum(X, S), axis=1)
+
+    def predict(self, X, method="histogram_intersection"):
+        # compute distances between X and all examples in the training set
+        if method == "histogram_intersection":
+            dists = self._histogram_intersection(X, self.X_train)
+        else:
+            dists = self._hellinger(X, self.X_train)
+
+        # Return the indices of k neighbors
+        idx = np.argsort(dists)[:self.k]
+
+        # Extract the labels of the k nearest neighbor training samples
+        k_closest_classes = self.y_train[idx]
+
+        # return the most common class label, the indices of the closest neighbors, and the distances to the closest neighbors
+        return np.argmax(np.bincount(k_closest_classes)), idx, dists[idx]
---
+++ runKNN.py
@@ -0,0 +1,59 @@
+from NearestNeighbor import KNearestNeighbor
+from HOG import HOG
+import numpy as np
+import argparse
+import glob
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-k", "--knn", type=int, default=3, help="k value for kNN")
+parser.add_argument("-m", "--method", type=str, default="histogram_intersection", help="method for computing distance")
+parser.add_argument("-d", "--dataset", type=str, help="dataset to use")
+
+args = vars(parser.parse_args())
+
+def main():
+    # Crawl through dataset and extract features using HOG
+    # Keep track of filenames and labels
+
+    # Training set
+    train_filenames = []
+    train_labels = []
+    for filename in glob.glob(args["dataset"] + "/DB_Images_Neg/*.bmp"):
+        train_filenames.append(filename)
+        train_labels.append(0)
+    for filename in glob.glob(args["dataset"] + "/DB_Images_Pos/*.bmp"):
+        train_filenames.append(filename)
+        train_labels.append(1)
+
+    # Testing set
+    test_filenames = []
+    test_labels = []
+    for filename in glob.glob(args["dataset"] + "/Test_Images_Neg/*.bmp"):
+        test_filenames.append(filename)
+        test_labels.append(0)
+    for filename in glob.glob(args["dataset"] + "/Test_Images_Pos/*.bmp"):
+        test_filenames.append(filename)
+        test_labels.append(1)
+    
+    # Extract features from training set
+    train_features = []
+    for filename in train_filenames:
+        train_features.append(HOG(filename))
+    
+    # Extract features from testing set
+    test_features = []
+    for filename in test_filenames:
+        test_features.append(HOG(filename))
+
+    # Train kNN classifier
+    knn = KNearestNeighbor(k=args["knn"])
+    knn.fit(np.array(train_features), np.array(train_labels))
+
+    # Test kNN classifier
+    predictions = []
+    for feature in test_features:
+        predictions.append(knn.predict(feature, method=args["method"])[0])
+    
+
+if __name__ == "__main__":
+    main()
